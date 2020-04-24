ea6e99a71577c2a20996f5550b1b0b231fd0c6bf
nastasiadotlic <nastasiadotlic@gmail.com>
2020-04-24 11:24:43 +0200

    add js page

--- src/loader.js
+++ src/loader.js
@@ -1,17 +1,8 @@
 /*
 LOADER
 */
-var purple = "#C2BDFB";
-var currentColor = purple;
-
-//var home = document.querySelector('.home');
 
 setTimeout(function() {
-    
     document.location.href="screen/homepage.html";
     clearTimeout();
 }, 5000);
-
-// setTimeout(function(e) {
-//     alert("say hello");
-// }, 4000);
