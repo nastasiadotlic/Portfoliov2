38e3a6a65058acc039ff639afe3a205e0deb3914
nastasiadotlic <nastasiadotlic@gmail.com>
2020-05-04 16:23:12 +0200

    page project modifs

--- src/script.js
+++ src/script.js
@@ -8,7 +8,6 @@ var rellax = new Rellax('.homepage__subtitle');
 var rellax = new Rellax('.scroll');
 var rellax = new Rellax('.description');
 var rellax = new Rellax('.works');
-var rellax = new Rellax('.work');
 
 
 
