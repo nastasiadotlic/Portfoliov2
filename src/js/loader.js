d7c2a8787d2ed4e47bb670f2f1b9560bcc517b82
nastasiadotlic <nastasiadotlic@gmail.com>
2020-05-20 15:25:32 +0200

    path

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,6 +3,6 @@ LOADER
 */
 
 setTimeout(function() {
-    document.location.href="~/../screen/homepage.html";
+    document.location.pathname="../screen/homepage.html";
     clearTimeout();
 }, 4000);
