84bee24f68732707fd2f04e9cf9a10efc190f306
nastasiadotlic <nastasiadotlic@gmail.com>
2020-05-21 14:51:20 +0200

    cursor

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,10 @@
 
 
 var purple = "#C2BDFB";
+var blue = "#C0E9FF";
 
 //var currentColor = purple;
 
 localStorage.setItem('currentColor', purple);
+innerDot.style.backgroundColor = currentColor == purple ? blue : purple;
+innerCircle.style.borderColor = currentColor == purple ? blue : purple;
